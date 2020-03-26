b25de16d7399e82ad6df50faa81d10f8cbdfb8a2
Celso Bufano <cbufano@kapatron.com.br>
2020-03-25 22:21:44 -0300

    first commit

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,65 @@
+const connection = require('../database/connection');
+
+module.exports = {
+
+  async index(request, response) {
+    const { page = 1} = request.query;
+
+    const [count] = await connection('incidents').count();
+    console.log(count);
+    const incidents = await connection('incidents')
+    .limit(5)
+    .join('ongs','ongs.id','=','incidents.ong_id')
+    .offset((page-1)*5)
+    .select('incidents.*','ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf');
+    response.header('X-Total-Count' , count['count(*)']);
+    return response.json(incidents);
+
+  },
+
+
+  async create(request, response) {
+    const body = request.body;
+    console.log(body);
+
+    const { title, description, value, ativo } = request.body;
+
+    const ong_id = request.headers.authorization;
+    console.log(ong_id);
+
+    const [id] = await connection('incidents').insert({
+      title,
+      description,
+      value,
+      ong_id,
+      ativo,
+    });
+
+    return response.json({ id });
+
+  },
+
+  async delete(request, response) {
+    const { id } = request.params;
+    const ong_id = request.headers.authorization;
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
+
+      if (incident == null) {
+        return response.status(404).json({ error: 'Id not Found' });
+      }
+
+    
+    if (incident.ong_id != ong_id) {
+      return response.status(401).json({ error: 'Operation not permitted.' });
+    }
+    await connection('incidents').where('id',id).delete();
+    return response.status(204).send();
+
+
+  }
+
+
+};
---
+++ backend/src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());  // Informo o express que vai receber dados no 
+                          // corpo das requisições  no formato json
+
+
+app.use(routes);
+
+
+app.listen(3333);
+
+
+/**
+ * Rota / Recurso
+ */
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Busca/Listar uma informação do Back-end
+ * POST: Cria uma informação no Back-end
+ * PUT: Alterar uma informação no Back-end
+ * DELETE: Deletar uma informação no Back-end
+ */
+/**
+ * Tipo de parãmetros:
+ * 
+ * Query Params : Parâmetros nomeados enviados na rota após "?" (Filtros , pagainação)
+ * Rote Params  : Parâmetros utilizados para identificar recusrsos
+ * Request Budy : Corpo de requisição, utilizados para criar ou alterar recursos
+ */
+/**
+ * SQL: MySql, SQLIte,PostgreeSQL, Oracle , Nicrosoft SQL Server
+ * NoSql: MongoDB, CouchDB, etc.
+ */
+/**
+ * Driver: Select * from users
+ * Query Builder : table('users').select('*').where()
+ * Vamos usar o KNextJS
+ */
+
+
+
